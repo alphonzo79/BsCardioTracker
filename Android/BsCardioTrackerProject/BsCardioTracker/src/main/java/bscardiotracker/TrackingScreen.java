ae8ed8d1eda45294700c8333ba216d0ca34606a3
Joe Rowley (Laptop) <joe.rowley@bodybuilding.com>
2013-06-16 18:31:39 -0600

    Starting out with Tracking Screen

--- Android/BsCardioTrackerProject/BsCardioTracker/src/main/java/bscardiotracker/TrackingScreen.java
+++ Android/BsCardioTrackerProject/BsCardioTracker/src/main/java/bscardiotracker/TrackingScreen.java
@@ -1,6 +1,14 @@
 package bscardiotracker;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class TrackingScreen extends Activity {
+public class TrackingScreen extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tracking_screen);
+    }
 }
