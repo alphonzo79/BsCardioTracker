6a0b1d7c1cc70aff5c2ebfda5d98dd601596c7ec
Joe Rowley (Laptop) <joe.rowley@bodybuilding.com>
2013-06-17 23:20:57 -0600

    Working on getting map to display for the first time

--- Android/BsCardioTrackerProject/BsCardioTracker/src/main/java/com/bscardiotracker/TrackingScreen.java
+++ Android/BsCardioTrackerProject/BsCardioTracker/src/main/java/com/bscardiotracker/TrackingScreen.java
@@ -1,17 +1,19 @@
 package com.bscardiotracker;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
-import com.bscardiotracker.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 public class TrackingScreen extends FragmentActivity {
-    private GoogleMap mapFrag;
+    private GoogleMap map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,12 +25,27 @@ public class TrackingScreen extends FragmentActivity {
     public void onResume() {
         super.onResume();
 
-        if(mapFrag == null) {
-            mapFrag = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        setUpMap();
+    }
 
-            if(mapFrag == null) {
+    private void setUpMap() {
+        if(map == null) {
+            map = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
+            if(map == null) {
                 Toast.makeText(this, "There was a problem getting the map", Toast.LENGTH_LONG).show();
             }
+        }
+
+        if(map != null) {
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            UiSettings options = map.getUiSettings();
+            options.setCompassEnabled(true);
+            options.setZoomControlsEnabled(false);
+            options.setZoomGesturesEnabled(true);
+            options.setRotateGesturesEnabled(true);
+            options.setScrollGesturesEnabled(false);
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(43.635, -116.605), 16, 0, 0)));
         }
     }
 }
